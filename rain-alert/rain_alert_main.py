4d4edce54172d30f3b7d0d6744254fdc5139501d
Vinzce Yadao <yadaovinzce@gmail.com>
2022-10-19 13:16:00 -0400

    Create rain_alert_main.py

---
+++ rain-alert/rain_alert_main.py
@@ -0,0 +1,13 @@
+import requests
+
+api_key = '3c6f9af205d7167ed3c450104c244cc2'
+
+weather_params = {
+    'lat': 30.438255,
+    'lon': -84.280731,
+    'appid': api_key
+}
+
+response = requests.get('https://api.openweathermap.org/data/2.5/onecall', params=weather_params)
+print(response.json())
+
